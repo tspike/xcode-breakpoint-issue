a5bd47bc6b105e44ada385b14779421c7609b3f8
Tres Spicher <tspicher@walmartlabs.com>
2020-07-07 15:40:28 -0700

    Initial Commit

---
+++ BreakpointIssue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreakpointIssue
+//
+//  Created by Tres Spicher on 7/7/20.
+//  Copyright © 2020 Tres Spicher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
