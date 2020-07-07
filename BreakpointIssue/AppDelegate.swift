7ee251ea53e75a63dca4b352aa98fcb30c658d8e
Tres Spicher <tspicher@walmartlabs.com>
2020-07-07 15:43:11 -0700

    initial commit

--- BreakpointIssue/AppDelegate.swift
+++ BreakpointIssue/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let plusFive = [1,2,3].map { $0 + 5 }
+        print(plusFive)
         return true
     }
 
